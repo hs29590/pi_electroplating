a96b222dfe6cf490b450068f5cc51fde6e035158
Harshit Sureka <harshit.sureka@gmail.com>
2017-10-27 10:04:07 +0530

    adding coordinates with extended dobbot magician end effector

--- magician.py
+++ magician.py
@@ -11,34 +11,35 @@ if len(available_ports) == 0:
 z_up = 20
 z_down = 0
 
-home_xyz = [215, 0, 100];
+home_xyzr = [215, 0, 100, 0];
 
-Beakers = [ [0, 0, 0],      #0 dummy beaker for numbering
-            [-140, -165, 5], #1 # -135deg
-            #[-112, -271, 5],  #2 #  -112.6deg
-            [-95, -284, 5],  #2 #  -112.6deg
-            [0, -211, 5], #3 # -90deg
-            [112, -271, 5], #4 # -67.6deg
-            [149, -149, 5], #5 # -45deg
-            [270, -102, 5],  #6 # -22.6deg
-            [210, 5, 5],    #7 # 0deg
-            [271, 113, 5],  #8 # 22.4deg
-            [149, 149, 5],   #9 # 45deg
-            [112, 270, 5],    #10 # 67.4deg
-            [-189, 223, 5], #11 # 130deg
-            [-25, 209, 5]]; #12 #97deg
+#x,y,z,r
+
+Beakers = [ [0, 0, 0, 0],      #0 dummy beaker for numbering
+            [-16.3, -214.8, 0, -197.2], #1 # -135deg
+            [39.67, -290.2, 0, -185.15],  #2 #  -112.6deg
+            [119.48, -173.56, 0, -158.4], #3 # -90deg
+            [185.99, -193, 0, -122], #4 # -67.6deg
+            [187.12, -66.2, 0, -96.4], #5 # -45deg
+            [272.41, -13.6, 0, -79.9],  #6 # -22.6deg
+            [211, -74.7, 0, 30.3],    #7 # 0deg
+            [271.42, 34.54, 0, 32.1],  #8 # 22.4deg
+            [198.72, 70.09, 0, 65],   #9 # 45deg
+            [193.53, 206.47, 0, 92.4],    #10 # 67.4deg
+            [-75.58, 242.89, 0, 152.8], #11 # 130deg
+            [75.54, 183.83, 0, 113.22]]; #12 #97deg
 
 
 device = Dobot(port=available_ports[0])
 time.sleep(1)
 
-device.go(home_xyz[0], home_xyz[1], home_xyz[2]);
+device.go(home_xyzr[0], home_xyzr[1], home_xyzr[2], home_xyzr[3]);
 
     
 def up_down_beaker(id):
     print ("Doing beaker " + str(id) + " now");
-    device.go(Beakers[id][0], Beakers[id][1], z_up);
-    device.go(Beakers[id][0], Beakers[id][1], z_down);
+    device.go(Beakers[id][0], Beakers[id][1], z_up, Beakers[id][3]);
+    device.go(Beakers[id][0], Beakers[id][1], z_down, Beakers[id][3]);
 
 time.sleep(1)
 device.speed(30)
@@ -57,6 +58,6 @@ up_down_beaker(10);
 #set Rh voltage
 #up_down_beaker(11);
 up_down_beaker(12);
-device.go(home_xyz[0], home_xyz[1], home_xyz[2]);
+device.go(home_xyzr[0], home_xyzr[1], home_xyzr[2], home_xyzr[3]);
 
 device.close()
